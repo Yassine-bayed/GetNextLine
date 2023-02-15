ca8473cd6a2e6dfd0344233a71faf2d2aab0cffd
Yassine Bayed <Yassine.bayed12@gmail.com>
2023-02-15 20:26:36 +0100

    maybe the main file is good now

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybayed <ybayed@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/27 14:12:32 by ybayed            #+#    #+#             */
-/*   Updated: 2023/02/08 15:49:36 by ybayed           ###   ########.fr       */
+/*   Created: 2023/02/08 14:12:32 by ybayed            #+#    #+#             */
+/*   Updated: 2023/02/15 20:25:24 by ybayed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,56 @@ char *read_line(int fd, char *static_buffer)
 	size = 1;
 	return (static_buffer);
 }
+char *fixed_line(char *static_buffer)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if(!static_buffer)
+		return (NULL);
+	while(static_buffer[i] && static_buffer[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	if(!line)
+		return (NULL);
+	i = 0;
+	while(static_buffer[i] && static_buffer[i] != '\n')
+	{
+		line[i] = static_buffer[i];
+		i++;
+	}
+	if(static_buffer[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+char *next_line(char *static_buffer)
+{
+	int		i;
+	int		j;
+	char 	*tab;
+
+	i = 0;
+	while (static_buffer[i] && static_buffer != '\n')
+		i++;
+	if(!static_buffer)
+	{
+		free(static_buffer);
+		return(NULL);
+	}
+	tab = (char *)malloc(sizeof(char) * (ft_strlen(static_buffer) - i + 1));
+	if(!tab)
+		return(NULL);
+	i++;
+	j = 0;
+	while(static_buffer[i])
+		tab[j++] = static_buffer[i++];
+	tab[j] = '\0';
+	free(static_buffer);
+	return (tab);
+}
+
 
 char *get_next_line(int fd)
 {
@@ -42,14 +92,20 @@ char *get_next_line(int fd)
     return (ln);
 }
 
-// int main()
+// int main(int argc, char **argv)
 // {
-//     int fd;
-//     char *line = "test";
+// 	int		fd;
+// 	char	*line;
 
-//     fd = open("ddd", O_RDONLY);
-
-//         line = get_next_line(fd);
-//         printf("%s", line);
-//     return (0);
+// 	if (argc == 2)
+// 	{
+// 		fd = open(argv[1], O_RDONLY);
+// 		while (get_next_line(fd) > 0)
+// 		{
+// 			printf("%s", line);
+// 			free(line);
+// 		}
+// 		close(fd);
+// 	}
+// 	return (0);
 // }
